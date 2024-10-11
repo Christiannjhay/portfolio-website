e11a9262091746d49353b6bbc3b3e1f9232b5b4a
Christiannjhay <sugarolchristian8@gmail.com>
2024-10-11 12:39:19 +0800

    updated padding and font size

--- src/components/HomePage/LandingText.tsx
+++ src/components/HomePage/LandingText.tsx
@@ -12,8 +12,8 @@ export default function LandingText() {
       <div className="text-sm sm:text-lg md:text-xl lg:text-2xl pt-2 w-2/3 font-GeomRegular text-red-500 text-center font-light">
         <h2>I make the complex simple</h2>
       </div>
-      <div className=" w-full py-10 flex justify-center items-center h-full">
-        <button className="learn-button relative text-white px-6 py-2 rounded-3xl font-GeomLight bg-red-500 hover:bg-red-700">
+      <div className=" w-full flex justify-center items-center h-full">
+        <button className="learn-button text-xs md:text-lg relative text-white px-6 py-2 rounded-3xl font-GeomLight bg-red-500 hover:bg-red-700">
           Learn more about me
         </button>
       </div>
